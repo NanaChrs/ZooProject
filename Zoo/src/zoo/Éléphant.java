672221fa8798ac40cca9bde8d878c19a5e5be727
Story DeWeese <sdeweese@scu.edu>
2018-11-17 14:40:24 +0100

    ajouté en colère

--- Zoo/src/zoo/Éléphant.java
+++ Zoo/src/zoo/Éléphant.java
@@ -9,6 +9,35 @@ package zoo;
  *
  * @author storyd
  */
-public class Éléphant {
+public class Éléphant extends Animal{
     
+    public Éléphant(){
+        sound = "*bruit d'éléphant*";
+    }
+    
+    @Override
+    public void eat(){
+        System.out.println("Éléphant: J'ai un apitit enorme ! Donnez-moi quelque chose à manger s'il vous plaît. N'importe quoi. \n Mmmm c'est delicieux ! Merci beaucoup !");
+        System.out.println("L'éléphant a bien mangé des grandes feuilles.");
+        faim = false;
+    }
+    
+    @Override
+    public void talk(String s){
+        System.out.println("Éléphant: " + sound + s);
+    }
+    
+ /*   @Override
+    public void interage(){
+        if(faim == true){
+            this.parle();
+            System.out.println("L'éléphant a faim. Il a un appétit enorme!");
+            this.parle(); 
+        }
+    }*/
+    
+    @Override
+    public String toString(){
+        return "Type d'animal = Éléphant. Nom = " + getName() + ". Sound = " + getSound() + ". Faim = " + getFaim() + ". Soif = " + getSoif() + ". Malade = " + getMalade() + ". Dormir = " + getDormir() + ". Jouer = " + getJouer() + ". En Colère = " + getEnColère() + ".";
+    }
 }
