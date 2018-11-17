779fc40a5c327b7eaa0f48b039ff9d2a9d7a4ef4
Story DeWeese <sdeweese@scu.edu>
2018-11-17 14:30:25 +0100

    ajouté des autres animaux

---
+++ Zoo/src/zoo/Zèbra.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zoo;
+
+/**
+ *
+ * @author storyd
+ */
+public class Zèbra extends Animal{
+    
+    public Zèbra(){
+        sound = "*bruit de zèbra*";
+    }
+    
+    @Override
+    public void eat(){
+        System.out.println("Le zèbra a bien mangé des herbes.");
+        faim = false;
+    }
+    
+    @Override
+    public void talk(String s){
+        System.out.println("Zébra: " + sound + s);
+    }
+    
+  /*  @Override
+    public void interage(){
+        if(faim == true){
+            this.parle();
+            System.out.println("Le zébra a faim.");
+            this.parle(); 
+        }
+    }*/
+    
+    @Override
+    public String toString(){
+        return "Type d'animal = Zèbra. Nom = " + getName() + ". Sound = " + getSound() + ". Faim = " + getFaim() + ". Soif = " + getSoif() + ". Malade = " + getMalade() + ". Dormir = " + getDormir() + ". Jouer = " + getJouer() + ".";
+    }
+}
