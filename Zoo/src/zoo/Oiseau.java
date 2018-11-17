31b3c1dbf8dbbad93ff8157841c85f6db32ddbf5
Story DeWeese <sdeweese@scu.edu>
2018-11-17 14:31:27 +0100

    ajouté des autres animaux

---
+++ Zoo/src/zoo/Oiseau.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zoo;
+
+/**
+ *
+ * @author storyd
+ */
+public class Oiseau extends Animal{
+    
+    public Oiseau(){
+        sound = "Chirp! Chirp! Chirp!";
+    }
+    
+     @Override
+    public void eat(){
+        System.out.println("Le oiseau a bien mangé des graines.");
+        faim = false;
+    }
+    
+    @Override
+    public void talk(String s){
+        System.out.println("Oiseau: " + sound + s);
+    }
+    
+   /* @Override
+    public void interage(){
+        if(faim == true){
+            this.parle();
+            System.out.println("Le oiseau a faim. Jetez des graines pour lui.");
+            this.parle(); 
+        }
+    }*/
+    
+    @Override
+    public String toString(){
+        return "Type d'animal = Oiseau. Nom = " + getName() + ". Sound = " + getSound() + ". Faim = " + getFaim() + ". Soif = " + getSoif() + ". Malade = " + getMalade() + ". Dormir = " + getDormir() + ". Jouer = " + getJouer() + ".";
+    }
+}
