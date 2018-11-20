6fc1f73c94dd77ef2240bb441dda15e82fdbaa29
Story DeWeese <sdeweese@scu.edu>
2018-11-20 14:43:57 +0100

    JavaDocs

--- Zoo/src/zoo/Lion.java
+++ Zoo/src/zoo/Lion.java
@@ -1,38 +1,71 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package zoo;
+
+import java.util.Random;
 
 /**
  *
  * @author storyd
  */
 public class Lion extends Animal {
-    public Lion(){
+    
+    /**
+     * This constructor initializes the following attributes randomly:
+     * faim, soif, malade, jouer, encolère, malade
+     * This allows each experience with the program to be unique.
+     * 
+     * @param n the String for the animal's name attribute
+     * 
+     * However, it would not make sense for an animal to be either playing or
+     * angry while it is sleeping. To make this zoo more interactive (one
+     * where not few animals are sleeping) after the attributes are assigned,
+     * if both the jouer and encolère attribute are false, the dormir attribute
+     * is randomly assigned to true or false. In the case that either jouer 
+     * or encolère is true, dormir is automatically assigned to false.
+     * 
+     * Finally, the sound attribute is unique to this animal.
+     */
+    public Lion(String n){
+        name = n;
+        Random random = new Random();
+        faim = random.nextBoolean();   
+        soif = random.nextBoolean();    
+        malade = random.nextBoolean(); 
+        jouer = random.nextBoolean();
+        encolère = random.nextBoolean();
+        malade = random.nextBoolean();
+        if(jouer == false && encolère == false)
+            dormir = random.nextBoolean();
+        else
+            dormir = false;
         sound = "Roar!";
     }
     
+    /**
+     * This method allows a user to change the animal's faim attribute
+     * from true to false
+     * 
+     */
     @Override
     public void eat(){
         System.out.println("Le lion a bien mangé du viande.");
         faim = false;
     }
     
+    /**
+     * This method allows the animal to speak by printing out the type of animal, it's name, and a given String
+     * @param s the String the animal will say
+     * 
+     */
     @Override
     public void talk(String s){
         System.out.println(this.getClass().getSimpleName()+"("+this.name+"): "+s+ sound);
     }
     
- /*   @Override
-    public String interact(){
-        if(faim == true){
-            return "Je suis un faim lion et je vais essaie de manger des gens si personne ne me donne pas du viande vitement!";
-            
-        }
-    }*/
-    
+    /**
+     * This method displays all of the animal's attributes
+     * @return Class, name, sound, faim, soif, malade, dormir, jouer, and encolère,
+     */
     @Override
     public String toString(){
         return "Type d'animal = Lion. Nom = " + getName() + ". Sound = " + getSound() + ". Faim = " + getFaim() + ". Soif = " + getSoif() + ". Malade = " + getMalade() + ". Dormir = " + getDormir() + ". Jouer = " + getJouer() + ". En Colère = " + getEnColère() + ".";
