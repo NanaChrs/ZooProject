ff0d1bc4022c55b139aacb152e227b1d28980cc4
Story DeWeese <sdeweese@scu.edu>
2018-11-24 09:52:34 +0100

    changé toString d'être en anglais.

--- Zoo/src/zoo/Monkey.java
+++ Zoo/src/zoo/Monkey.java
@@ -67,7 +67,7 @@ public class Monkey extends Animal {
      */
     @Override
     public String toString(){
-        return "Type d'animal = Singe. Nom = " + getName() + ". Sound = " + getSound() + ". Faim = " + getFaim() + ". Soif = " + getSoif() + ". Malade = " + getMalade() + ". Dormir = " + getDormir() + ". Jouer = " + getJouer() + ". En Colère = " + getEnColère() + ".";
+        return "Type of animal = "+this.getClass().getSimpleName()+" Name = " + getName() + ". Sound = " + getSound() + ". Hungry = " + getFaim() + ". Thirsty = " + getSoif() + ". Sick = " + getMalade() + ". Sleeping = " + getDormir() + ". Playing = " + getJouer() + ". Angry = " + getEnColère() + ".";
     }
     
 }
