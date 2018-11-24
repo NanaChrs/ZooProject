09b1b41bfd47a5015fe9f715c58144ef5609edc8
Story DeWeese <sdeweese@scu.edu>
2018-11-24 09:52:18 +0100

    changé toString d'être en anglais.

--- Zoo/src/zoo/Giraffe.java
+++ Zoo/src/zoo/Giraffe.java
@@ -67,7 +67,7 @@ public class Giraffe extends Animal{
      */
     @Override
     public String toString(){
-        return "Type d'animal = Girafe. Nom = " + getName() + ". Sound = " + getSound() + ". Faim = " + getFaim() + ". Soif = " + getSoif() + ". Malade = " + getMalade() + ". Dormir = " + getDormir() + ". Jouer = " + getJouer() + ". En Colère = " + getEnColère() + ".";
+        return "Type of animal = "+this.getClass().getSimpleName()+" Name = " + getName() + ". Sound = " + getSound() + ". Hungry = " + getFaim() + ". Thirsty = " + getSoif() + ". Sick = " + getMalade() + ". Sleeping = " + getDormir() + ". Playing = " + getJouer() + ". Angry = " + getEnColère() + ".";
     }
     
 }
